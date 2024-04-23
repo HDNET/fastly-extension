efe77b93f8925e15d58d9edc7999775946bf6a9a
Tim Spiekerkötter <tim.spiekerkoetter@hdnet.de>
2024-04-23 18:43:25 +0200

    wip: TYPO3 12 compatibility

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,14 +5,11 @@ use HDNET\CdnFastly\Cache\FastlyBackend;
 defined('TYPO3') || die();
 
 $boot = static function (): void {
-    if (empty($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['CdnFastly'] ?? null)) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['CdnFastly'] = [
-            'backend' => FastlyBackend::class,
-            'groups' => [
-                'fastly',
-            ],
-        ];
-    }
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['CdnFastly'] ??= [];
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['CdnFastly']['backend'] ??= FastlyBackend::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['CdnFastly']['groups'] ??= [
+        'fastly',
+    ];
 };
 
 $boot();
